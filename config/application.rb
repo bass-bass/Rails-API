3163584b5244d8b93376b612ec1f80f47c922e0e
bass-bass <bounceback0505@gmail.com>
2021-06-11 21:29:43 +0900

    update

--- config/application.rb
+++ config/application.rb
@@ -24,6 +24,10 @@ module App
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
 
+    #追加
+    config.assets.compile = true
+    config.assets.initialize_on_precompile=false
+    
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
